dfb429e9ca8de40ac3e8e0c08219675737efc4a6
Navaneeth1797 <rnavaneethk1797@gmail.com>
2024-01-29 15:01:55 +0530

    created netlify file and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // Importing necessary modules and components from React and react-router-dom
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";
+import NavBar from "./Components/NavBar.jsx";
 import All from "./Components/All.jsx";
 import Fsd from "./Components/Fsd.jsx";
 import Ds from "./Components/Ds.jsx";
